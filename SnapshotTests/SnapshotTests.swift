37b93076be3da072f5b4b9ca5ac702754e440527
ShengHuaWu <fantasy0404@gmail.com>
2017-03-13 15:46:52 +0800

    Integreate FBSnapshotTestCase.

--- SnapshotTests/SnapshotTests.swift
+++ SnapshotTests/SnapshotTests.swift
@@ -6,30 +6,27 @@
 //  Copyright © 2017 ShengHuaWu. All rights reserved.
 //
 
-import XCTest
+import FBSnapshotTestCase
+@testable import Snapshot
 
-class SnapshotTests: XCTestCase {
-    
+class SnapshotTests: FBSnapshotTestCase {
+    // MARK: - Override Methods
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        recordMode = false
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // MARK: - Enabled Tests
+    func testViewController() {
+        let vc = ViewController()
+        
+        vc.view.backgroundColor = UIColor.brown
+        
+        FBSnapshotVerifyView(vc.view)
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
